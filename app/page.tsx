136753f29a80c2eef23148f3ed3729f46e046e8c
Amir Reza Tamandani <dadartm@gmail.com>
2024-01-15 14:14:15 +0330

    build: custom color pallet, add shad button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 
 export default function Home() {
   return (
-<p>this it an empty p</p>
-  )
+    <main
+      className="flex flex-col items-center justify-center 
+      h-full
+    "
+    >
+      <div className="space-y-6 text-center">
+        <h1
+          className="text-6xl font-semibold 
+                     drop-shadow-md"
+        >
+          Auth me up !
+        </h1>
+        <p className="text-lg">Let's get in</p>
+      </div>
+    </main>
+  );
 }
