1cd4c6dbe0da948c6d7c4179317dc4c3851c93d5
Amir Reza Tamandani <dadartm@gmail.com>
2024-01-20 22:24:49 +0330

    ops : password hashed

--- actions/register.ts
+++ actions/register.ts
@@ -1,10 +1,30 @@
 "use server";
 import { RegisterSchema } from "@/schema";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
+
 export const register = async (value: z.infer<typeof RegisterSchema>) => {
   const ValidatedFields = RegisterSchema.safeParse(value);
   if (!ValidatedFields.success) {
     return { error: "invalid fields!" };
   }
-  return { success: "email sent!" };
+  const { email, password, name } = ValidatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+ 
+  if (existingUser) {
+    return { error: "Emails is taken" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  return { success: "User Created!" };
 };
